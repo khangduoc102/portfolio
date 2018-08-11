b598e2035d1f1ca2c894f02c319fba0f25c722ba
hoang <hoang_lp_97@yahoo.com>
2018-08-11 13:34:31 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,66 @@
 import React from 'react';
-
+import FaMapMarker from 'react-icons/lib/fa/map-marker';
+import MdMail from 'react-icons/lib/md/mail';
+import MdCall from 'react-icons/lib/md/call';
+import FaLinkedinSquare from 'react-icons/lib/fa/linkedin-square';
+import FaGithub from 'react-icons/lib/fa/github';
+import data from './../data.json';
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div className="app-footer">
-                <div className="contact-form">
-                    <p className="sub-header">Get in touch</p>
-                    <div className="line"></div>
-                    <form>
-                        <p className="text">Name*</p>
-                        <input type="text" size="50"/>
-                        <p className="text">Email*</p>
-                        <input type="text" size="50"/>
-                        <p className="text">Message*</p>
-                        <textarea rows="5" cols="60"/>
-                        <button className="btn send-btn" type="submit">
-                            <p className="sub-title">Send</p>
-                        </button>
-                    </form>
-                </div>
-                <div className="contact-places">
+            <div className="container-fluid app-footer">
+                <div className="row">
+                    <div className="col-8 contact-form">
+                        <p className="sub-header">Get in touch</p>
+                        <div className="line"></div>
+                        <form>
+                            <div className="form-group">
+                                <p className="text">Name*</p>
+                                <input className="form-control" type="text" size="50"/>
+                            </div>
+                            <div className="form-group">
+                                <p className="text">Email*</p>
+                                <input className="form-control" type="text" size="50"/>
+                            </div>
+                            <div className="form-group">
+                                <p className="text">Message*</p>
+                                <textarea className="form-control" rows="6" cols="60"/>
+                            </div>
+                            <button className="btn send-btn" type="submit">
+                                <p className="sub-title">Send</p>
+                            </button>
+                        </form>
+                    </div>
+                    <div className="col-4 contact-places">
+                        <p className="sub-header">Contact</p>
+                        <div className="line"></div>
+                        <div className="form-group">
+                            <div className="address-block">
+                                <h3><FaMapMarker /></h3>
+                                <p>{ data.contact.address }</p>
+                            </div>
+                            <div className="address-block">
+                                <h3><MdMail /></h3>
+                                <p>{ data.contact.email }</p>
+                            </div>
+                            <div className="address-block">
+                                <h3><MdCall /></h3>
+                                <p>{ data.contact.phone }</p>
+                            </div>
+                            <div className="address-block">
+                                <h3><FaLinkedinSquare /></h3>
+                                <p>{ data.contact.linkedin }</p>
+                            </div>
+                            <div className="address-block">
+                                <h3><FaGithub /></h3>
+                                <p>{ data.contact.github }</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="credit">
+                        <p>Made by Hoang Le. Designed by Ly Dang. </p>
+                    </div>
                 </div>
             </div>
         )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import IoBonfire from 'react-icons/lib/io/bonfire';
 import FaAngleRight from 'react-icons/lib/fa/angle-right';
 import FaAngleLeft from 'react-icons/lib/fa/angle-left';
-
 import data from './../data.json';
 
 export default class Home extends React.Component {
@@ -50,9 +49,9 @@ export default class Home extends React.Component {
                 <div className="info">
                     <div className="intro">
                         <div className="intro-block">
-                            <p className="sub-header">Hi, I am Hoang Le</p>
+                            <p className="sub-header">{data.intro.title}</p>
                             <div className="line"></div>
-                            <p className="text">Lorem ipsum dolor sit amet, cu pro dicit torquatos contentiones, in cum solet iisque praesent. Sea ex quem esse natum, eos nisl luptatum et, nostro eruditi id vim. Brute omnium disputando eum id, putent delicata sea te, at reque mandamus appellantur sea. Mei et clita prodesset, ne pro nonumy repudiandae ullamcorper.</p>
+                            <p className="text">{ data.intro.description }</p>
                             <button className="btn rm1">
                                 <p className="sub-title">Read more <FaAngleRight /></p>
                             </button>
@@ -69,27 +68,29 @@ export default class Home extends React.Component {
                             <div className="left" onClick={this.handleLeft}>
                                 <p className="header"><FaAngleLeft /></p>
                             </div>
-                            <div className="row">
-                               {
-                                    data.projects.map((prj, index) => {
-                                        if(this.state.showedPrj.includes(index)){
-                                            return (
-                                                <div className="slide-list-element" key={index}>
-                                                    <div className="img-block">
-                                                        <img src={prj.img} alt="alt" />
+                            <div className="container">
+                                <div className="row">
+                                {
+                                        data.projects.map((prj, index) => {
+                                            if(this.state.showedPrj.includes(index)){
+                                                return (
+                                                    <div className="slide-list-element" key={index}>
+                                                        <div className="img-block">
+                                                            <img src={prj.img} alt="alt" />
+                                                        </div>
+                                                        <div className="text-block">
+                                                            <p className="sub-title prj-title">{prj.name}</p>
+                                                            <p className="text prj-des">{prj.description}</p>
+                                                        </div>
+                                                        <button className="btn">
+                                                            <p className="sub-title">Read more <FaAngleRight /></p>
+                                                        </button>
                                                     </div>
-                                                    <div className="text-block">
-                                                        <p className="sub-title prj-title">{prj.name}</p>
-                                                        <p className="text prj-des">{prj.description}</p>
-                                                    </div>
-                                                    <button className="btn">
-                                                        <p className="sub-title">Read more <FaAngleRight /></p>
-                                                    </button>
-                                                </div>
-                                            )
-                                        }
-                                    })
-                               }
+                                                )
+                                            }
+                                        })
+                                }
+                                </div>
                             </div>
                             <div className="right" onClick={this.handleRight}>
                                 <p className="header"><FaAngleRight /></p>
@@ -103,7 +104,33 @@ export default class Home extends React.Component {
                         <p className="sub-header">Education</p>
                         <div className="line"></div>
                         <div className="show-list">
-                            <div className="show-list-element">
+                            <div className="container show-list-element">
+                                <div className="row">
+                                    <div className="col-8 p-0">
+                                        <div className="ava">
+                                            <img alt="vamk" className="ava-img"/>
+                                        </div>
+                                        <div className="description">
+                                            <p className="sub-title">{data.education[0].name}</p>
+                                            <p className="text">{data.education[0].degree}</p>
+                                            <p className="mini-text">{data.education[0].time}</p>
+                                        </div>
+                                    </div>
+                                    <div className="col-4 p-0">
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <div className="dropdown">
+                                                    <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Courses</button>
+                                                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                        <a className="dropdown-item">{data.education[0].courses[0].name}</a>
+                                                        <a className="dropdown-item">{data.education[0].courses[1].name}</a>
+                                                        <a className="dropdown-item">{data.education[0].courses[2].name}</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
